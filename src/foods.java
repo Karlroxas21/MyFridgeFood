7855bde7ee6d12f669f4982b44d1c8b2c8e4560e
Karlroxas21 <karlmarxroxas1@gmail.com>
2021-05-14 08:06:46 +0800

    update

--- src/foods.java
+++ src/foods.java
@@ -59,135 +59,145 @@ public class foods extends JFrame {
     JRadioButton foodE0 = new JRadioButton("Food E0");
 
 
-    JPanel selectFood = new JPanel();
+    JPanel selectPanel = new JPanel();
 
 
     public foods() {
-        //guide only
-        selectFood.setBackground(Color.red);
-        selectFood.setBounds(10,10,534,545);
+        //guides only
+        selectPanel.setLayout(null);
+        selectPanel.setBounds(10,10,524,700);
+        selectPanel.setBorder(BorderFactory.createEtchedBorder());
+        selectPanel.setLayout(new GridLayout(10, 5));
 
 
         //end of guide
-        foodA1.setBounds(10, 10, 100, 50);
-        foodA2.setBounds(10,65,100,50);
-        foodA3.setBounds(10,120,100,50);
-        foodA4.setBounds(10,175,100,50);
-        foodA5.setBounds(10,230,100,50);
-        foodA6.setBounds(10, 285, 100, 50);
-        foodA7.setBounds(10,340,100,50);
-        foodA8.setBounds(10,395,100,50);
-        foodA9.setBounds(10,450,100,50);
-        foodA0.setBounds(10,505,100,50);
+        foodA1.setFocusable(false);
+        foodA2.setFocusable(false);
+        foodA3.setFocusable(false);
+        foodA4.setFocusable(false);
+        foodA5.setFocusable(false);
+        foodA6.setFocusable(false);
+        foodA7.setFocusable(false);
+        foodA8.setFocusable(false);
+        foodA9.setFocusable(false);
+        foodA0.setFocusable(false);
 
-        foodB1.setBounds(115, 10, 100, 50);
-        foodB2.setBounds(115, 65, 100, 50);
-        foodB3.setBounds(115, 120, 100, 50);
-        foodB4.setBounds(115, 175, 100, 50);
-        foodB5.setBounds(115, 230, 100, 50);
-        foodB6.setBounds(115, 285, 100, 50);
-        foodB7.setBounds(115, 340, 100, 50);
-        foodB8.setBounds(115, 395, 100, 50);
-        foodB9.setBounds(115, 450, 100, 50);
-        foodB0.setBounds(115, 505, 100, 50);
 
-        foodC1.setBounds(220, 10, 100, 50);
-        foodC2.setBounds(220, 65, 100, 50);
-        foodC3.setBounds(220, 120, 100, 50);
-        foodC4.setBounds(220, 175, 100, 50);
-        foodC5.setBounds(220, 230, 100, 50);
-        foodC6.setBounds(220, 285, 100, 50);
-        foodC7.setBounds(220, 340, 100, 50);
-        foodC8.setBounds(220, 395, 100, 50);
-        foodC9.setBounds(220, 450, 100, 50);
-        foodC0.setBounds(220, 505, 100, 50);
+        foodB1.setFocusable(false);
+        foodB2.setFocusable(false);
+        foodB3.setFocusable(false);
+        foodB4.setFocusable(false);
+        foodB5.setFocusable(false);
+        foodB6.setFocusable(false);
+        foodB7.setFocusable(false);
+        foodB8.setFocusable(false);
+        foodB9.setFocusable(false);
+        foodB0.setFocusable(false);
 
-        foodD1.setBounds(325, 10, 100, 50);
-        foodD2.setBounds(325, 65, 100, 50);
-        foodD3.setBounds(325, 120, 100, 50);
-        foodD4.setBounds(325, 175, 100, 50);
-        foodD5.setBounds(325, 230, 100, 50);
-        foodD6.setBounds(325, 285, 100, 50);
-        foodD7.setBounds(325, 340, 100, 50);
-        foodD8.setBounds(325, 395, 100, 50);
-        foodD9.setBounds(325, 450, 100, 50);
-        foodD0.setBounds(325, 505, 100, 50);
 
-        foodE1.setBounds(430, 10, 100, 50);
-        foodE2.setBounds(430, 65, 100, 50);
-        foodE3.setBounds(430, 120, 100, 50);
-        foodE4.setBounds(430, 175, 100, 50);
-        foodE5.setBounds(430, 230, 100, 50);
-        foodE6.setBounds(430, 285, 100, 50);
-        foodE7.setBounds(430, 340, 100, 50);
-        foodE8.setBounds(430, 395, 100, 50);
-        foodE9.setBounds(430, 450, 100, 50);
-        foodE0.setBounds(430, 505, 100, 50);
+        foodC1.setFocusable(false);
+        foodC2.setFocusable(false);
+        foodC3.setFocusable(false);
+        foodC4.setFocusable(false);
+        foodC5.setFocusable(false);
+        foodC6.setFocusable(false);
+        foodC7.setFocusable(false);
+        foodC8.setFocusable(false);
+        foodC9.setFocusable(false);
+        foodC0.setFocusable(false);
+
+
+        foodD1.setFocusable(false);
+        foodD2.setFocusable(false);
+        foodD3.setFocusable(false);
+        foodD4.setFocusable(false);;
+        foodD5.setFocusable(false);
+        foodD6.setFocusable(false);
+        foodD7.setFocusable(false);
+        foodD8.setFocusable(false);
+        foodD9.setFocusable(false);
+        foodD0.setFocusable(false);
+
+
+        foodE1.setFocusable(false);
+        foodE2.setFocusable(false);
+        foodE3.setFocusable(false);
+        foodE4.setFocusable(false);
+        foodE5.setFocusable(false);
+        foodE6.setFocusable(false);
+        foodE7.setFocusable(false);
+        foodE8.setFocusable(false);
+        foodE9.setFocusable(false);
+        foodE0.setFocusable(false);
+
         //add here
-        this.add(foodA1);
-        this.add(foodA2);
-        this.add(foodA3);
-        this.add(foodA4);
-        this.add(foodA5);
-        this.add(foodA6);
-        this.add(foodA7);
-        this.add(foodA8);
-        this.add(foodA9);
-        this.add(foodA0);
+        selectPanel.add(foodA1);
+        selectPanel.add(foodA2);
+        selectPanel.add(foodA3);
+        selectPanel.add(foodA4);
+        selectPanel.add(foodA5);
+        selectPanel.add(foodA6);
+        selectPanel.add(foodA7);
+        selectPanel.add(foodA8);
+        selectPanel.add(foodA9);
+        selectPanel.add(foodA0);
 
-        this.add(foodB1);
-        this.add(foodB2);
-        this.add(foodB3);
-        this.add(foodB4);
-        this.add(foodB5);
-        this.add(foodB6);
-        this.add(foodB7);
-        this.add(foodB8);
-        this.add(foodB9);
-        this.add(foodB0);
+        selectPanel.add(foodB1);
+        selectPanel.add(foodB2);
+        selectPanel.add(foodB3);
+        selectPanel.add(foodB4);
+        selectPanel.add(foodB5);
+        selectPanel.add(foodB6);
+        selectPanel.add(foodB7);
+        selectPanel.add(foodB8);
+        selectPanel.add(foodB9);
+        selectPanel.add(foodB0);
 
-        this.add(foodC1);
-        this.add(foodC2);
-        this.add(foodC3);
-        this.add(foodC4);
-        this.add(foodC5);
-        this.add(foodC6);
-        this.add(foodC7);
-        this.add(foodC8);
-        this.add(foodC9);
-        this.add(foodC0);
+        selectPanel.add(foodC1);
+        selectPanel.add(foodC2);
+        selectPanel.add(foodC3);
+        selectPanel.add(foodC4);
+        selectPanel.add(foodC5);
+        selectPanel.add(foodC6);
+        selectPanel.add(foodC7);
+        selectPanel.add(foodC8);
+        selectPanel.add(foodC9);
+        selectPanel.add(foodC0);
 
-        this.add(foodD1);
-        this.add(foodD2);
-        this.add(foodD3);
-        this.add(foodD4);
-        this.add(foodD5);
-        this.add(foodD6);
-        this.add(foodD7);
-        this.add(foodD8);
-        this.add(foodD9);
-        this.add(foodD0);
+        selectPanel.add(foodD1);
+        selectPanel.add(foodD2);
+        selectPanel.add(foodD3);
+        selectPanel.add(foodD4);
+        selectPanel.add(foodD5);
+        selectPanel.add(foodD6);
+        selectPanel.add(foodD7);
+        selectPanel.add(foodD8);
+        selectPanel.add(foodD9);
+        selectPanel.add(foodD0);
 
-        this.add(foodE1);
-        this.add(foodE2);
-        this.add(foodE3);
-        this.add(foodE4);
-        this.add(foodE5);
-        this.add(foodE6);
-        this.add(foodE7);
-        this.add(foodE8);
-        this.add(foodE9);
-        this.add(foodE0);
+        selectPanel.add(foodE1);
+        selectPanel.add(foodE2);
+        selectPanel.add(foodE3);
+        selectPanel.add(foodE4);
+        selectPanel.add(foodE5);
+        selectPanel.add(foodE6);
+        selectPanel.add(foodE7);
+        selectPanel.add(foodE8);
+        selectPanel.add(foodE9);
+        selectPanel.add(foodE0);
+
 
         //do not move/ edit this.
         this.setTitle("My Fridge Food");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-        this.setSize(960, 960);
+        this.setSize(1360, 760);
         this.setLayout(null);
         this.setVisible(true);
 
-        //this.add(selectFood);
+
+        this.add(selectPanel);
+
 
 
 
--- src/main.java
+++ src/main.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 
 public class main {
     public static void main(String[] args){
+
         new foods();
     }
 }
