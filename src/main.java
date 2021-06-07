93258cf2c7ea15871055e8d0f81ebff6b85e0bbc
Karlroxas21 <karlmarxroxas1@gmail.com>
2021-06-07 12:08:07 +0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -4,25 +4,6 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args){
-
-
         LaunchPage launchMe = new LaunchPage();
-
-//        TO CREATE FIL
-
-//        String file = "E:\\karl123.txt";
-//        File myObj = new File(file);
-//
-//        try {
-//            FileWriter fileWriter = new FileWriter(file);
-//           myObj.createNewFile();
-//           fileWriter.write("test123");
-//           fileWriter.close();
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-
-
     }
 }
